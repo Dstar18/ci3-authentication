cda5b5cba9ba0ecd27c832a6f2f1ac8d022e4b61
Dstar18 <dedybintang18@gmail.com>
2023-06-12 13:03:23 +0700

    authentication & oauth2

--- application/config/custom.php
+++ application/config/custom.php
@@ -1,7 +1,7 @@
 <?php
 
 // ---------- Config OAuth2 Google ----------
-$config['client_id']        = '1029634648358-k1toqfdfthp34t9l9df7d99qghcp6gqv.apps.googleusercontent.com';
-$config['client_secret']    = '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';
+$config['client_id']        = 'input client ID';
+$config['client_secret']    = 'input client secret';
 $config['redirect_uri']     = 'http://localhost/ci3-authentication/Login/authgoogle/';
 $config['redirect_uri_register']     = 'http://localhost/ci3-authentication/Register/insert_authGoogle/';
